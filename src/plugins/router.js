b641e95dd3737751325e0e761f00e89f289b1c98
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2026-07-30 17:56:28 +0100

    GSPS-540 Add initial Payment Hub endpoint stub

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,11 +1,12 @@
 import { health } from '#/routes/health.js'
-import { example } from '#/routes/example.js'
+import { payRequestConsumer } from '#/routes/consumer/pay-request.js'
 
 export const router = {
   plugin: {
     name: 'router',
     register: (server) => {
-      server.route([health].concat(example))
+      server.route([health])
+      server.register([payRequestConsumer])
     }
   }
 }
